e2fb78d264b4294851ce284cba333c1a73d383d3
Maxime Cauvin <maxime.cauvin@epitech.eu>
2019-02-16 03:45:13 +0100

    Updating versioning in code

--- BeatSaberCustomExit/BeatSaberCustomExit/Plugin.cs
+++ BeatSaberCustomExit/BeatSaberCustomExit/Plugin.cs
@@ -11,7 +11,7 @@ namespace BeatSaberCustomExit
     public class Plugin : IPlugin
     {
         public string Name => "BeatSaberCustomExit";
-        public string Version => "1.0.0";
+        public string Version => "1.0.1";
 
         public void OnApplicationStart()
         {

5344107a0221854492cb3d508b46557d441fcba2
Maxime Cauvin <maxime.cauvin@epitech.eu>
2019-03-21 10:04:55 +0900

    Fixing assembly informations

--- BeatSaberCustomExit/0.13/Plugin.cs
+++ BeatSaberCustomExit/0.13/Plugin.cs
@@ -14,7 +14,7 @@ namespace BeatSaberCustomExit
     public class Plugin : IPlugin
     {
         public string Name => "BeatSaberCustomExit";
-        public string Version => "1.1.0";
+        public string Version => "1.1.1";
 
         public static void QuitGame()
         {
